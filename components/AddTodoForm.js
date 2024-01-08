78b3565a29313a3ef52fe5b7b5a8913e1102d3c0
GYStudio <100853145+GYStudio@users.noreply.github.com>
2024-01-08 17:20:57 +0300

    Basic File Information

---
+++ components/AddTodoForm.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AddTodoForm = () => {
+  return (
+    <View>
+      <Text>AddTodoForm</Text>
+    </View>
+  )
+}
+
+export default AddTodoForm
+
+const styles = StyleSheet.create({})
---
+++ screens/TodoDetailsScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TodoDetailsScreen = () => {
+  return (
+    <View>
+      <Text>TodoDetailsScreen</Text>
+    </View>
+  )
+}
+
+export default TodoDetailsScreen
+
+const styles = StyleSheet.create({})
